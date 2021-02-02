3eb9a230ca7d4048a7073c8f0ee781332e12ba2c
Thiago dos Passos <64388418+thiagosteps227@users.noreply.github.com>
2021-02-02 15:49:58 +0000

    Update WineDAO.java

--- src/com.ait.wine/WineDAO.java
+++ src/com.ait.wine/WineDAO.java
@@ -7,6 +7,8 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.mysql.jdbc.PreparedStatement;
+
 
 public class WineDAO {
 
@@ -29,6 +31,72 @@ public class WineDAO {
 		}
         return list;
     }
+    
+    public Wine findById(int id) {
+    	String sql = "SELECT * FROM wine WHERE id=?";
+    	Wine wine = null;
+    	Connection c = null;
+    	try {
+    		c = ConnectionHelper.getConnection();
+    		PreparedStatement ps = (PreparedStatement) c.prepareStatement(sql);
+    		ps.setInt(1, id);
+    		ResultSet rs = ps.executeQuery();
+    		if(rs.next()) {
+    			wine = processRow(rs);
+    		}
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    		throw new RuntimeException(e);
+    	} finally {
+    		ConnectionHelper.close(c);
+    	}
+    	return wine;
+    }
+    
+    public List<Wine> findByName(String name) {
+    	List<Wine> list = new ArrayList<Wine>();
+    	Connection c= null;
+    	String sql = "SELECT * FROM wine AS e WHERE UPPER(name) LIKE ? ORDER BY name";
+    	try {
+    		c = ConnectionHelper.getConnection();
+    		java.sql.PreparedStatement ps = c.prepareStatement(sql);
+    		ps.setString(1, "%"+name.toUpperCase()+"%");
+    		ResultSet rs = ps.executeQuery();
+    		while(rs.next()) {
+    			list.add(processRow(rs));
+    		}
+    	} catch (SQLException e){
+    		e.printStackTrace();
+    		throw new RuntimeException(e);
+    	} finally {
+    		ConnectionHelper.close(c);
+    	}
+    	return list;
+    }
+    
+    public List<Wine> findByCountryAndGrapes(String country, String grapes){
+    	List<Wine> list = new ArrayList<Wine>();
+    	Connection c= null;
+    	String sql = "SELECT * FROM wine AS e WHERE UPPER(country) LIKE ? AND UPPER(grapes) LIKE ? "
+    			+ "ORDER BY name";
+    	try {
+    		c = ConnectionHelper.getConnection();
+    		PreparedStatement ps = (PreparedStatement) c.prepareStatement(sql);
+    		ps.setString(1, "%"+country.toUpperCase()+"%");
+    		ps.setString(2, "%"+grapes.toUpperCase()+"%");
+    		ResultSet rs = ps.executeQuery();
+    		while(rs.next()) {
+    			list.add(processRow(rs));
+    		}
+    	} catch (SQLException e) {
+    		e.printStackTrace();
+    		throw new RuntimeException(e);
+    	} finally {
+    	ConnectionHelper.close(c);
+    	}
+    	return list;
+    }
+
 
     
    
