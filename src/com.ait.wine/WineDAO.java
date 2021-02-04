7bb7db614ae4a89d1433cadbeb29225dc1a85608
Thiago dos Passos <64388418+thiagosteps227@users.noreply.github.com>
2021-02-04 11:15:13 +0000

    Update WineDAO.java

--- src/com.ait.wine/WineDAO.java
+++ src/com.ait.wine/WineDAO.java
@@ -96,6 +96,77 @@ public class WineDAO {
     	}
     	return list;
     }
+    
+    public Wine create(Wine wine) {
+    	Connection c= null;
+    	java.sql.PreparedStatement ps = null;
+    	try {
+    		c = ConnectionHelper.getConnection();
+    		ps = c.prepareStatement("INSERT INTO wine "
+    				+ "(name, grapes, country, region, year, picture, description)"
+    				+ "VALUES(?,?,?,?,?,?,?)", 
+    			new String[] { "ID"});
+    		ps.setString(1, wine.getName());
+    		ps.setString(2, wine.getGrapes());
+    		ps.setString(3, wine.getCountry());
+    		ps.setString(4, wine.getRegion());
+    		ps.setString(5, wine.getYear());
+    		ps.setString(6, wine.getPicture());
+    		ps.setString(7, wine.getDescription());
+    		ps.executeUpdate();
+    		ResultSet rs = ps.getGeneratedKeys();
+    		rs.next();
+    		int id = rs.getInt(1);
+    		wine.setId(id);
+    	} catch (Exception e){
+    		e.printStackTrace();
+    		throw new RuntimeException(e);
+    	} finally {
+    		ConnectionHelper.close(c);
+    	}
+    	
+    	return wine;
+    }
+    
+    public Wine update(Wine wine) {
+    	Connection c = null;
+    	try {
+    		c = ConnectionHelper.getConnection();
+    		PreparedStatement ps = (PreparedStatement) c.prepareStatement("UPDATE wine SET name=?, grapes=?, country=?,"
+    				+ "region=?, year=?, picture=?, description=? WHERE id=?");
+    		ps.setString(1, wine.getName());
+    		ps.setString(2, wine.getGrapes());
+    		ps.setString(3, wine.getCountry());
+    		ps.setString(4, wine.getRegion());
+    		ps.setString(5, wine.getYear());
+    		ps.setString(6, wine.getPicture());
+    		ps.setString(7, wine.getDescription());
+    		ps.setInt(8, wine.getId());
+    		ps.executeUpdate();
+    	} catch (SQLException e) {
+    		e.printStackTrace();
+    		throw new RuntimeException (e);
+    	} finally {
+    		ConnectionHelper.close(c);
+    	}
+    	return wine;
+    }
+    
+    public boolean remove(int id) {
+    	Connection c= null;
+    	try {
+    		c = ConnectionHelper.getConnection();
+    		PreparedStatement ps = (PreparedStatement) c.prepareStatement("DELETE FROM wine WHERE id =?");
+    		ps.setInt(1, id);
+    		int count = ps.executeUpdate();
+			return count == 1;
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    		throw new RuntimeException (e);
+    	} finally {
+    		ConnectionHelper.close(c);
+    	}
+    }
 
 
     
