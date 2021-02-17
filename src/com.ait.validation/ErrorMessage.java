0ae485bfa855029f5436817b462571e88dfbd034
Thiago dos Passos <64388418+thiagosteps227@users.noreply.github.com>
2021-02-17 12:59:13 +0000

    Create ErrorMessage.java

---
+++ src/com.ait.validation/ErrorMessage.java
@@ -0,0 +1,18 @@
+package com.ait.validation;
+
+public class ErrorMessage {
+	String errorMessage;
+	
+	public ErrorMessage(String message) {
+		this.errorMessage=message;
+	}
+	
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	/*
+	 * public void setErrorMessage(String message) { this.errorMessage = message; }
+	 */
+
+}
